6983b38af14f6acbed1fad393273e154b09ac6c3
Sonteru4 <sanjanaonteru404@gmail.com>
2024-06-03 10:55:11 +0600

    process song data

--- etl.py
+++ etl.py
@@ -21,3 +21,32 @@ def create_spark_session():
     return spark
 
 
+def process_song_data(spark, input_data, output_data):
+    """
+    Description:
+        Process the songs data files and create extract songs table and artist table data from it.
+
+    :param spark: a spark session instance
+    :param input_data: input file path
+    :param output_data: output file path
+    """
+
+    # get filepath to song data file
+    song_data = input_data + "song_data/*/*/*/*"
+
+    # read song data file
+    df = spark.read.json(song_data, mode='PERMISSIVE', columnNameOfCorruptRecord='corrupt_record').drop_duplicates()
+
+    # extract columns to create songs table
+    songs_table = df.select("song_id","title","artist_id","year","duration").drop_duplicates()
+
+    # write songs table to parquet files partitioned by year and artist
+    songs_table.write.parquet(output_data + "songs/", mode="overwrite", partitionBy=["year","artist_id"])
+
+    # extract columns to create artists table
+    artists_table = df.select("artist_id","artist_name","artist_location","artist_latitude","artist_longitude").drop_duplicates()
+
+    # write artists table to parquet files
+    artists_table.write.parquet(output_data + "artists/", mode="overwrite")
+
+
