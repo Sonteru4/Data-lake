6a8e0d3666e522a9ebfb9ac240f3df7f7842b41a
Sonteru4 <sanjanaonteru404@gmail.com>
2024-06-03 10:53:55 +0600

    elt.py created

---
+++ etl.py
@@ -0,0 +1,9 @@
+import configparser
+from datetime import datetime
+import os
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import udf, col, monotonically_increasing_id
+from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format, dayofweek
+from pyspark.sql.types import *
+
+
